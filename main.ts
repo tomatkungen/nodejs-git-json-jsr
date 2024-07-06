a8a746ce7f4d69122b309d7748914ee9e8089a84
tomatkungen <kimkarlsson@Kims-MacBook-Pro-2.local>
2024-07-06 10:35:30 +0200

    chore: try add jsr package manager

---
+++ main.ts
@@ -0,0 +1,14 @@
+// mod.ts
+/**
+ * A module providing a function to greet people.
+ * @module
+ */
+
+/**
+ * Greet a person.
+ *
+ * @param name The name of the person to greet.
+ */
+export function greet(name: string) {
+  console.log(`Hello, ${name}!`);
+}
